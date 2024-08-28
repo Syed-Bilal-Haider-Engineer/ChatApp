4fcd4d04b53b906b3c250511cee11ffe486bc67b
syed bilal haider <bilaldev151214@gmail.com>
2024-08-28 16:33:39 -0700

    Design chat box , icons , profile status and bottom send input.

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,23 @@
-import React, {lazy, Suspense} from "react";
+import React from "react";
 import Chats from "./Chats";
+import { Stack, Box, useTheme } from '@mui/material';
+import Converstation from "../../components/Converstation";
 
 const GeneralApp = () => {
+  const theme = useTheme();
+  
   return (
     <>
-     <Chats/>
+      <Stack direction="row" sx={{ width: '100%' }}>
+        <Chats />
+        <Box sx={{ 
+            height: '100%', 
+            width: "calc(100vw - 420px)",
+            backgroundColor: theme.palette.mode === 'light' ? '#fff' : theme.palette.background.default,
+          }}>
+          <Converstation />
+        </Box>
+      </Stack>
     </>
   );
 };
